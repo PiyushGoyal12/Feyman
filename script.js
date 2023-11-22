9e6fb89bd52655f8ba809a4ef2b956c58d50f834
Piyush Goyal <2004piyushgoyal@gmail.com>
2023-11-22 17:03:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("nav-options-list");
+    if (x.style.display === "grid") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "grid";
+    }
+}
